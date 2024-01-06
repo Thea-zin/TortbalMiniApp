a388ab06dee3dc79f6a1fc1cdadb5aa8173dc137
oenghokleng <oeng.hokleng07@gmail.com>
2024-01-06 16:02:47 +0700

    initial project

---
+++ android/app/src/main/kotlin/com/example/thortbal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thortbal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Constants/tb_textsize.dart
@@ -0,0 +1,7 @@
+class TBTextSize {
+  static double xsmall = 8.0;
+  static double small = 10.0;
+  static double medium = 12.0;
+  static double large = 16.0;
+  static double xlarge = 24.0;
+}
