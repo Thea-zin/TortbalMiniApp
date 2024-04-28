925749abb59935260d9f7a1244b40abe1988de27
Thea-zin <bansothea20@kit.edu.kh>
2024-04-28 10:57:35 +0700

    change password screen

--- lib/Views/Profile/profile.screen.dart
+++ lib/Views/Profile/profile.screen.dart
@@ -10,6 +10,7 @@ import 'package:thortbal/Views/Profile/Book-History/book_history.screen.dart';
 import 'package:thortbal/Views/Profile/Bookmarks/bookmark.screen.dart';
 import 'package:thortbal/Views/Profile/Refund/refund.screen.dart';
 import 'package:thortbal/Views/Profile/Review/review.screen.dart';
+import 'package:thortbal/Views/authentication/changePassword.screen.dart';
 
 class TBProfileScreen extends StatefulWidget {
   const TBProfileScreen({Key? key}) : super(key: key);
@@ -144,7 +145,7 @@ class _TBProfileScreenState extends State<TBProfileScreen> {
                     profileRow(
                       title: "Change Password",
                       assetName: TBIcons.tbLock,
-                      child: const TBReviewScreen(),
+                      child: const TBChangePassword(),
                     ),
                     profileRow(
                       title: "Manager Account",
