66093d54b9ed6cb07ac680d9fb156eed7e4e8a6a
Thea-zin <bansothea20@kit.edu.kh>
2024-05-20 13:41:49 +0700

    write user authentication api service

--- lib/data/datasource/remote/api_service.dart
+++ lib/data/datasource/remote/api_service.dart
@@ -1,13 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:thortbal/core/error/exceptions.dart';
 import 'package:thortbal/core/utils/typedefs.dart';
 
 class ApiService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   Future<void> login(String username, String password) {
     throw UnimplementedError();
   }
 
   Future<void> signInWithGmail(String username, String password) {
     throw UnimplementedError();
+  }
+  ///Login with email and password 
+  Future<User?>signInwithEmailAndPassword(String email, String password) async{
+    try{
+      UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    
+    } on FirebaseAuthException catch (e){
+      print("error");
+      return null;
+    }
+  }
+
+  ///Register User with email and password 
+   Future<User?>createUserwithEmailAndPassword(String email, String password) async{
+    try{
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    
+    } on FirebaseAuthException catch (e){
+      print("error");
+      return null;
+    }
+  }
+
+  ///Sign out
+  Future<void>signOut() async{
+    await _firebaseAuth.signOut();
   }
 
   ResultFuture<void> signInWithFacebook() async {
